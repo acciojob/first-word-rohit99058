a3fed8dc120d353b6d0b885c38471912baf1aab0
Rohit Kumar <patelrr2552001@gmail.com>
2024-11-15 15:25:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+let arr = s.trim.split(" ")
+	return arr[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
